f0affafc5921217d3e7c0926159ee4b4b1f38432
Jakub Gałecki <goathy.dev@gmail.com>
2023-03-11 16:35:37 +0000

    feat(tokenize): handle simple case of negative numbers

--- tokenizer.go
+++ tokenizer.go
@@ -10,8 +10,10 @@ func isOperator(s string) bool {
 	return s == opLeftPar || s == opRightPar || s == opAdd || s == opMulti || s == opDiv || s == opPow || s == opSub
 }
 
+// RESOURCE: https://stackoverflow.com/questions/46861254/infix-to-postfix-for-negative-numbers#46861656
 func Tokenize(expression string) []string {
 	var (
+		merge  = false
 		ns     = queue.New[string]()
 		output = make([]string, 0)
 	)
@@ -21,6 +23,21 @@ func Tokenize(expression string) []string {
 		case blank:
 			continue
 		case opLeftPar, opRightPar, opAdd, opMulti, opDiv, opPow, opSub:
+
+			if ns.Peek() == empty && e == opAdd {
+				continue
+			}
+
+			if ns.Peek() == opSub && e == opAdd {
+				continue
+			}
+
+			if ns.Peek() == empty && e == opSub {
+				ns.Enqueue(e)
+				merge = true
+				continue
+			}
+
 			if !isOperator(ns.Peek()) && ns.Peek() != empty {
 				v := ns.Dequeue()
 				output = append(output, v)
@@ -33,6 +50,15 @@ func Tokenize(expression string) []string {
 
 			ns.Enqueue(e)
 		default:
+
+			if merge {
+				v := ns.Dequeue()
+
+				ns.Enqueue(fmt.Sprintf("%s%s", v, e))
+				merge = false
+				continue
+			}
+
 			if isOperator(ns.Peek()) {
 				v := ns.Dequeue()
 				output = append(output, v)
