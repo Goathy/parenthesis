e4d7da29723b6aa1c0b1eadd8834c323adc08335
Jakub Gałecki <goathy.dev@gmail.com>
2023-03-11 13:13:21 +0000

    feat(tokenize): tokenize infix expression
    * Without handling negative numbers.

--- tokenizer.go
+++ tokenizer.go
@@ -1,5 +1,58 @@
 package parenthesis
 
+import (
+	"fmt"
+
+	"github.com/Goathy/containers/queue"
+)
+
+func isOperator(s string) bool {
+	return s == opLeftPar || s == opRightPar || s == opAdd || s == opMulti || s == opDiv || s == opPow || s == opSub
+}
+
 func Tokenize(expression string) []string {
-	return []string{"53"}
+	var (
+		ns     = queue.New[string]()
+		output = make([]string, 0)
+	)
+
+	for _, exp := range expression {
+		switch e := string(exp); e {
+		case opLeftPar, opRightPar, opAdd, opMulti, opDiv, opPow, opSub:
+			if !isOperator(ns.Peek()) && ns.Peek() != "" {
+				v := ns.Dequeue()
+				output = append(output, v)
+			}
+
+			if isOperator(ns.Peek()) {
+				v := ns.Dequeue()
+				output = append(output, v)
+			}
+
+			// TODO: Remove type conversion
+			ns.Enqueue(e)
+		default:
+
+			// TODO: If e is number and Peek is opSub concat
+
+			if isOperator(ns.Peek()) {
+				v := ns.Dequeue()
+				output = append(output, v)
+				ns.Enqueue(e)
+			} else if !isOperator(ns.Peek()) {
+				v := ns.Dequeue()
+				ns.Enqueue(fmt.Sprintf("%s%s", v, e))
+			} else {
+				ns.Enqueue(e)
+			}
+		}
+	}
+
+	for !ns.IsEmpty() {
+		v := ns.Dequeue()
+
+		output = append(output, v)
+	}
+
+	return output
 }
